c7f46d461d0ea68dcf374e7073ccc136792276e6
kseny <ksenyavozm1407@gmail.com>
2025-05-24 20:45:30 +0500

    correct --cont argument

--- utils/argparser.py
+++ utils/argparser.py
@@ -5,7 +5,8 @@ def parse_train_args():
 
     parser.add_argument("--data", type=str, required=True, help="Path to root data")
     parser.add_argument("--epochs", type=int, required=True, help="Number of epochs")
-    parser.add_argument("--cont", action="store_true", help="Continue training")
+    parser.add_argument("--cont", type=str, required=False, default=None,
+                        help="Path to existing version folder (e.g., versions/v3) to continue training")
 
     return parser.parse_args()
 
