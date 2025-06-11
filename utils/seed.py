323f57064db16634e9cbbc2212c144c2ccb24c6f
kseny <ksenyavozm1407@gmail.com>
2025-06-11 15:46:55 +0500

    hide pycache

--- utils/seed.py
+++
@@ -1,7 +0,0 @@
-def set_seed(seed):
-    import random, torch, numpy as np
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    np.random.seed(seed)
-    random.seed(seed)
-    torch.backends.cudnn.deterministic = True
